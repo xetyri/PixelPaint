176e95aef4206441e43264c021f2e1755eec3a83
Роман Лещенко <xetyri2014@gmail.com>
2022-11-01 15:57:35 +0200

    Correcting code

--- js/script.js
+++ js/script.js
@@ -12,21 +12,8 @@ const mainColor = "rgb(124, 176, 170)";
 
 const MainEl = document.querySelector("#blockMain"); 
 const divEl = document.querySelector("#screen");
-screenElements(standart);
-
-for (let i = 0; i < colors.length; i++){
-    const buttonEl = document.createElement('div');
-    buttonEl.classList.add("block-main-el");
-    buttonEl.style.backgroundColor = colors[count++];
-    buttons.push(buttonEl);
-    buttonEl.addEventListener('click', () => {
-        checkedColor();
-        buttonEl.classList.add("block-main-highlight");
-        count = i;
-        // console.log(count);
-    });
-    MainEl.appendChild(buttonEl);
-}
+addScreenElements(standart);
+addToolbarColors();
 
 
 const clearButton = document.createElement('img');
@@ -34,25 +21,13 @@ clearButton.src = "./img/clear.svg";
 clearButton.alt = "clear";
 clearButton.classList.add("block-main-clear");
 MainEl.appendChild(clearButton);
-clearButton.addEventListener('click', () => {   
-    checkedColor();
-    triger = true;
-    clearButton.classList.add("block-clear-highlight");
-})
+clearButton.addEventListener('click', onCleanTool)
 
 const gridCheck = document.createElement("input");
 gridCheck.setAttribute("type", "checkbox");
 gridCheck.classList.add("grid-button");
 MainEl.appendChild(gridCheck);
-gridCheck.addEventListener('click', () => { 
-    if (gridCheck.checked) {
-        elements.map(element => element.classList.add("grid-change"));
-        gridCheckImg.classList.add("grid-button-img-activ");
-    } else {
-        elements.map(element => element.classList.remove("grid-change"));
-        gridCheckImg.classList.remove("grid-button-img-activ");
-    }
-})
+gridCheck.addEventListener('change', onCheckedGrid)
 
 const gridCheckImg = document.createElement("img");
 gridCheckImg.src = "./img/grid.svg";
@@ -63,54 +38,21 @@ const fillButton = document.createElement('img');
 fillButton.src = "./img/paint.svg";
 fillButton.classList.add("block-main-reload");
 MainEl.appendChild(fillButton);
-fillButton.addEventListener('click', () => { 
-    const colorFill = document.querySelector(".block-main-highlight");
-    if (colorFill === null) {
-        checkedColor();
-        alert("Choose a fill color.")
-    } else {
-        checkedColor();
-        elements.map(element => element.style.backgroundColor = colorFill.style.backgroundColor);
-        // console.log(colorFill.style.backgroundColor);
-    }
-})
+fillButton.addEventListener('click', onFillScreen) 
 
 const reloadButton = document.createElement('img');
 reloadButton.src = "./img/reload.svg";
 reloadButton.alt = "reload";
 reloadButton.classList.add("block-main-reload");
 MainEl.appendChild(reloadButton);
-reloadButton.addEventListener('click', () => { 
-    checkedColor();
-    elements.map(element => element.style.backgroundColor = mainColor);
-})
+reloadButton.addEventListener('click', onReloadScreen)
 
 const screenButton = document.createElement('img');
 screenButton.src = "./img/screen.svg";
 screenButton.alt = "reload";
 screenButton.classList.add("block-main-reload");
 MainEl.appendChild(screenButton);
-screenButton.addEventListener('click', () => { 
-    checkedColor();
-    if (confirm("ATTENTION ! ! !   SCREEN WILL CLEAN !")) {
-        if (targetWidth === 1) {
-            screenDelete(standart);
-            divEl.style.width = "1500px";
-            screenElements(max);
-            targetWidth = 2;
-        } else if (targetWidth === 2) {
-            screenDelete(max);
-            divEl.style.width = "600px";
-            screenElements(min);
-            targetWidth = 3;
-        } else {
-            screenDelete(min);
-            divEl.style.width = "1200px";
-            screenElements(standart);
-            targetWidth = 1;
-        }
-    }
-})
+screenButton.addEventListener('click', onChangeScreen)
 
 const downloadLink = document.createElement('a');
 downloadLink.classList.add("download-link");
@@ -118,16 +60,17 @@ downloadLink.setAttribute('id', "download");
 MainEl.appendChild(downloadLink);
 screenshotUpdate();
 downloadLink.addEventListener('focus', () => { 
-    checkedColor();
+    resetToolbar();
     downloadLink.onclick = screenshotUpdate();
 })
 downloadLink.addEventListener('touchend', () => { 
-    checkedColor();
+    resetToolbar();
     downloadLink.addEventListener('touchmove', () => screenshotUpdate())
     downloadLink.onclick = screenshotUpdate();
 })
 
-function screenElements(num) {  
+
+function addScreenElements(num) {  
     for (let i = 0; i < num; i++) {
         const divBlockEl = document.createElement('div');
         divBlockEl.classList.add("screen-el");
@@ -139,21 +82,43 @@ function screenElements(num) {
     }
 }
 
-function screenDelete(t) {
-    for (let i = 0; i < t; i++) {
+function addToolbarColors() {
+    for (let i = 0; i < colors.length; i++){
+        const buttonEl = document.createElement('div');
+        buttonEl.classList.add("block-main-el");
+        buttonEl.style.backgroundColor = colors[count++];
+        buttons.push(buttonEl);
+        buttonEl.addEventListener('click', () => {
+            resetToolbar();
+            buttonEl.classList.add("block-main-highlight");
+            count = i;
+            // console.log(count);
+        });
+        MainEl.appendChild(buttonEl);
+    }
+}
+
+function deleteScreen(e) {
+    for (let i = 0; i < e; i++) {
         divEl.removeChild(divEl.firstChild);
     }
 }
  
-const checkedColor = () => {
+const resetToolbar = () => {
     for (let j = 0; j < colors.length; j++) {
-            if (buttons[j].classList.contains("block-main-highlight")) {
-                triger = false;
-                buttons[j].classList.remove("block-main-highlight");
-            } 
-        }
-    triger = false;
+        if (buttons[j].classList.contains("block-main-highlight")) {
+            triger = false;
+            buttons[j].classList.remove("block-main-highlight");
+        } 
+    }
+    // triger = false;
     clearButton.classList.remove("block-clear-highlight");
+}
+
+function deleteGrid() {
+    elements.map(element => element.classList.remove("grid-change"));
+    gridCheckImg.classList.remove("grid-button-img-activ");
+    gridCheck.checked = false;
 }
 
 function screenshotUpdate() {
@@ -164,4 +129,63 @@ function screenshotUpdate() {
             $("#download").attr("download", "image.jpg").attr("href", newData);
         }
     });
+}
+
+function onCheckedGrid(event) {
+    if (event.currentTarget.checked) {
+        elements.map(element => element.classList.add("grid-change"));
+        gridCheckImg.classList.add("grid-button-img-activ");
+    } else {
+        deleteGrid();
+    }
+}
+
+function onChangeScreen() {
+    if (confirm("ATTENTION ! ! !   SCREEN WILL CLEAN !")) {
+        resetToolbar();
+        deleteGrid();
+        if (targetWidth === 1) {
+            deleteScreen(standart);
+            divEl.style.width = "1500px";
+            addScreenElements(max);
+            targetWidth = 2;
+        } else if (targetWidth === 2) {
+            deleteScreen(max);
+            divEl.style.width = "600px";
+            addScreenElements(min);
+            targetWidth = 3;
+        } else {
+            deleteScreen(min);
+            divEl.style.width = "1200px";
+            addScreenElements(standart);
+            targetWidth = 1;
+        }
+    }
+}
+
+function onFillScreen() {
+    const colorFill = document.querySelector(".block-main-highlight");
+    if (colorFill === null) {
+        resetToolbar();
+        alert("CHOOSE A FILL COLOR.")
+    } else {
+        if (confirm(`ATTENTION ! ! !   SCREEN WILL CHANGE COLOR !`)) {
+            resetToolbar();
+            elements.map(element => element.style.backgroundColor = colorFill.style.backgroundColor);
+            // console.log(colorFill.style.backgroundColor);
+        }
+    }
+}
+
+function onCleanTool() {
+    resetToolbar();
+    triger = true;
+    clearButton.classList.add("block-clear-highlight");
+}
+
+function onReloadScreen() {
+    if (confirm("ATTENTION ! ! !   SCREEN WILL CLEAN !")) {
+        resetToolbar();
+        elements.map(element => element.style.backgroundColor = mainColor);
+    }
 }
